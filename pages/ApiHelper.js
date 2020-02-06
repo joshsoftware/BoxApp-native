62987b2e5eb97acd401442ff51bdae36c3bedf45
25shwetak <25shwetakshasha.com>
2020-02-06 13:44:44 +0530

    API helper code modified

--- pages/ApiHelper.js
+++ pages/ApiHelper.js
@@ -1,4 +1,5 @@
 import React, { Component, useState, useEffect } from 'react';
+import qs from 'query-string';
 
 function getDefaultHeaders(){
   const defaultHeaders = {
@@ -8,30 +9,26 @@ function getDefaultHeaders(){
   return(defaultHeaders);
 }
 
-const ApiHelper = {
+export default function(endpoint, body = {}, query_parameter = {}, method = "GET", headers={}){
+  let url = `${process.env.API_KEY}/api/v1${endpoint}`;
 
-  // api: function(endpoint,body,method, customHeaders={}){
-
-  api: function(endpoint,body,method,query_parameter){
-
-    const url= `${process.env.API_KEY}/api/v1/${endpoint}${query_parameter}`;
-
-    fetch(url, {
-      method,
-      headers: {
-        ...getDefaultHeaders()
-        //...customHeaders()
-      },
-      body
-    })
-    .then((response)=> response.json())
-    .then((responseJson) => {
-      return responseJson;
-    })
-    .catch((error) => {
-      throw error;
-    });
-  }
+  if(method=="GET" && Object.keys(query_parameter).length > 0) {
+    url= `${url}?${qs.stringify(query_parameter)}`;
+  } 
+  
+  return fetch(url, {
+    method,
+    headers: {
+      ...getDefaultHeaders(),
+      ...headers
+    },
+    body
+  })
+  .then((response)=> response.json())
+  .then((responseJson) => {
+    return responseJson;
+  })
+  .catch((error) => {
+    throw error;
+  });
 }
-
-export default ApiHelper;
